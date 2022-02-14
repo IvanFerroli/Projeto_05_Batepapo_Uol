ee27e6393e491c0c5ea7ca942802c9baba235ace
IvanFerroli <88122854+IvanFerroli@users.noreply.github.com>
2022-02-14 16:27:31 -0300

    bugs corrigidos, funcionalidades ok

--- script.js
+++ script.js
@@ -1,23 +1,59 @@
 const messagesContainer = document.querySelector("main");
 var entryInput = null;
-var name = null;
-
-function getEventToTriggerStartApp() {
-
-    var userInput = document.getElementById("entry-input");
-    userInput.addEventListener("keyup", function(event) {
-        if (event.key === 'Enter') {
-
-            event.preventDefault();
-            document.getElementById("entry-button").click();
-
-        }
-    });
-}
+let userName = null;
 
 function startApp() {
-    enterParticipantName();
-    setInterval(getMessages, 3000);
+  enterParticipantName();
+  // setInterval(getMessages, 3000);
+}
+
+function enterParticipantName() {
+  let entryInput = document.querySelector("#entry-input");
+  let name = entryInput.value;
+  userName = name;
+  let participantPostObject = {
+    name: name,
+  };
+
+  if (name != null && name != undefined && name != "") {
+    const participantNamePost = axios.post(
+      "https://mock-api.driven.com.br/api/v4/uol/participants",
+      participantPostObject
+    );
+
+    participantNamePost.catch(postErrors);
+
+    participantNamePost.then(callSendStatusAndToggle);
+  } else {
+    alert("Por favor, digite um nome válido");
+  }
+}
+
+function postErrors() {
+  alert("O nome de usuário já está sendo usado, por favor informe outro.");
+}
+
+function callSendStatusAndToggle() {
+  setInterval(participantStatus, 5000);
+  EntryScreenToggle();
+  setInterval(getMessages, 3000);
+}
+
+function participantStatus() {
+  let participantPostObject = {
+    name: userName,
+  };
+
+  const participantStatusPost = axios.post(
+    "https://mock-api.driven.com.br/api/v4/uol/status",
+    participantPostObject
+  );
+  participantStatusPost.then(console.log("Status: Online"));
+
+  participantStatusPost.catch(() => {
+    alert("Você foi desconectado do servidor, por favor, entre novamente.");
+    window.location.reload();
+  });
 }
 
 function getMessages() {
@@ -29,23 +65,23 @@ function getMessages() {
 }
 
 function scrollIntoView() {
-    const toView = document.querySelector('main').lastElementChild;
-    toView.scrollIntoView();
+  const toView = document.querySelector("main").lastElementChild;
+  toView.scrollIntoView();
 }
 
 function loadMessages(answer) {
-    var entryInput = document.querySelector("#entry-input");
-    var name = entryInput.value
-    for (i = 0; i <= answer.data.length; i++) {
-        let type = answer.data[i].type;
-        let time = answer.data[i].time;
-        let from = answer.data[i].from;
-        let to = answer.data[i].to;
-        let text = answer.data[i].text;
+  var entryInput = document.querySelector("#entry-input");
+  messagesContainer.innerHTML = "";
+  var name = entryInput.value;
+  for (i = 0; i < answer.data.length; i++) {
+    let type = answer.data[i].type;
+    let time = answer.data[i].time;
+    let from = answer.data[i].from;
+    let to = answer.data[i].to;
+    let text = answer.data[i].text;
 
-        if(to == "Todos" || to == name || from == name){
-
-            var message = `
+    if (to == "Todos" || to == name || from == name) {
+      var message = `
             <div class="msg ${type}">
             <div class="message-inner-container">
             <span class="time">(${time})</span>
@@ -55,138 +91,53 @@ function loadMessages(answer) {
             </div>
             </div>
             `;
-            messagesContainer.innerHTML += message;
-            scrollIntoView();
-
-        }
+      messagesContainer.innerHTML += message;
+      scrollIntoView();
     }
-}
-        
-function enterParticipantName() {
-    var entryInput = document.querySelector("#entry-input");
-    var name = entryInput.value
-    console.log(name);
-    if (name != null) {
-
-        enterParticipants();    
-        
-    }else {
-        
-        alert("Por favor, digite um nome válido");
-        
-    }
-    
-    setInterval(console.log(name), 1000);
-    // toggle();
+  }
 }
 
-function toggle() {
-    mainToggle = document.querySelector(".main-page");
-    entryToggle = document.querySelector(".entry-screen");
-    mainToggle.classList.toggle("hidden");
-    entryToggle.classList.toggle("hidden");
-    console.log(participantPostObject.data.status);
-}
-
-function postErrors() {
-
-    // switch (new Date().getDay()) {
-    //     case 0:
-    //       day = "Sunday";
-    //       break;
-    //     case 1:
-    //       day = "Monday";
-    //       break;
-    //     case 2:
-    //        day = "Tuesday";
-    //       break;
-    //     case 3:
-    //       day = "Wednesday";
-    //       break;
-    //     case 4:
-    //       day = "Thursday";
-    //       break;
-    //     case 5:
-    //       day = "Friday";
-    //       break;
-    //     case 6:
-    //       day = "Saturday";
-    //   }
-
-    console.log(participantPostObject.data.status);
-
-}
-
-function enterParticipants() {
-    
-    var entryInput = document.querySelector("#entry-input");
-    var name = entryInput.value
-    var participantPostObject = {
-
-        name:   name
-
-    }
-    
-    const participantNamePost = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", participantPostObject);
-
-    participantNamePost.catch(postErrors);
-    
-    participantNamePost.then(callSendStatusAndToggle);
-}
-
-function callSendStatusAndToggle() {
-    console.log("callSendStatusAndToggle sendo chamada");
-    setInterval(participantStatus, 5000);
-    toggle();
-}
-
-function participantStatus(){
-
-    var entryInput = document.querySelector("#entry-input");
-    var name = entryInput.value
-    var participantPostObject = {
-
-        name:   name
-
-    }
-    
-    const participantStatusPost = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", participantPostObject);
-    participantStatusPost.then(
-        // console.log("Status: Online")
-        console.log(participantPost.data.status)
-        );
-    
+function EntryScreenToggle() {
+  mainToggle = document.querySelector(".main-page");
+  entryToggle = document.querySelector(".entry-screen");
+  mainToggle.classList.toggle("hidden");
+  entryToggle.classList.toggle("hidden");
 }
 
 function sendMessage() {
-    alert('O envio com enter tá funfando coroi')
-    let from = document.querySelector("#entry-input").value;
-    let to = "Todos";
-    let text = document.querySelector("#message-input").value;
-    let type = "message";
-    
-    let messagePostObject = {
-        from: `${from}`,
-        to: `${to}`,
-        text: `${text}`,
-        type: `${type}`
-    };
+  let from = userName;
+  let to = "Todos";
+  let text = document.querySelector("#message-input").value;
+  let type = "message";
 
-    const messagePost = axios.post('https://mock-api.driven.com.br/api/v4/uol/messages', messagePostObject);
+  let messagePostObject = {
+    from: `${from}`,
+    to: `${to}`,
+    text: `${text}`,
+    type: `${type}`,
+  };
 
-    messagePost.then(console.log("Mensagem enviada"));
-    messagePost.catch(postErrors);
+  const messagePost = axios.post(
+    "https://mock-api.driven.com.br/api/v4/uol/messages",
+    messagePostObject
+  );
+
+  document.querySelector("#message-input").value = "";
+  messagePost.then(loadMessages);
+  messagePost.catch(postErrors);
 }
 
-function getEventToTriggerSendMessages() {
+const inputMessage = document.querySelector("#message-input");
+inputMessage.addEventListener("keydown", function (event) {
+  if (event.code === "Enter") {
+    sendMessage();
+  }
+});
 
-    var messageInput = document.getElementById("message-input");
-    messageInput.addEventListener("keyup", function(event) {
-        if (event.key === 'Enter') {
+function asideMenuToggle() {
+  const aside = document.querySelector("aside");
+  const overlay = document.querySelector(".overlay");
 
-            event.preventDefault();
-            document.getElementById("send-button").click();
-
-        }
-    });
+  aside.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
 }
